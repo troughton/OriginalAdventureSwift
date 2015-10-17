9fc2f94f7d3e074eadc6d215036caa5cd7d925a0
Thomas Roughton <t.roughton@me.com>
2015-10-18 11:25:11 +1300

    Initial Commit

---
+++ OriginalAdventure/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OriginalAdventure
+//
+//  Created by Thomas Roughton on 18/10/15.
+//  Copyright © 2015 Thomas Roughton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
